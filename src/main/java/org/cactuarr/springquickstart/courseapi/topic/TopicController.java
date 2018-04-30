5564b2d6670ea5e25a354fb339ce3c9f99703245
Arjay Hipolito <arjayhipolito@Chrough.local>
2018-04-30 09:12:34 +0800

    First commit

---
+++ src/main/java/org/cactuarr/springquickstart/courseapi/topic/TopicController.java
@@ -0,0 +1,44 @@
+package org.cactuarr.springquickstart.courseapi.topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@Autowired
+	private TopicService topicService;
+	
+	@GetMapping("/topics")
+	public List<Topic> getAllTopics(){
+		return topicService.getAllTopics();
+	}
+	
+	@GetMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@PostMapping("/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	} 
+	
+	@PutMapping("/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @RequestBody Topic id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@DeleteMapping("/topics/{id}")
+	public void deleteTopic(@RequestBody Topic topic, @RequestBody Topic id) {
+		topicService.deleteTopic(id, topic);
+	}
+}
---
+++ src/main/java/org/cactuarr/springquickstart/courseapi/topic/TopicRepository.java
@@ -0,0 +1,9 @@
+package org.cactuarr.springquickstart.courseapi.topic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicRepository extends JpaRepository<Topic, String>{
+	
+	
+	
+}
